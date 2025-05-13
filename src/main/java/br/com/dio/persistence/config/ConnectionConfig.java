041fe5edf7b4aef9898982182a7acdf194795aea
Ravelos <86490406+Ravelos@users.noreply.github.com>
2025-05-13 00:23:11 +0000

    create set up files

--- src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -1,3 +1,15 @@
+package br.com.dio.persistence.config;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+
 public final class ConnectionConfig {
     public static Connection getConnection() throws SQLException {
         var url = "jdbc:mysql://localhost/board";
